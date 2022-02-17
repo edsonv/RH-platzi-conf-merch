ce393c4a186ab5d9a43948fccc04493bf3ed4d10
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-17 17:03:27 -0500

    ADD: maquetación y estilos para lista de productos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import initialState from '../initialState';
+import Products from '../components/Products';
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <Products products={initialState.products} />
   )
 }
 
