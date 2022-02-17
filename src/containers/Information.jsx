703cf500db5e541e28fe014fc3d2ae6b00bca571
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-17 13:35:34 -0500

    ADD: componentes principales Se agregan los componenetes básicos de nuestra app

---
+++ src/containers/Information.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Information() {
+  return (
+    <h1>Information</h1>
+  )
+}
+
+export default Information;
---
+++ src/containers/Success.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Success() {
+  return (
+    <h1>Success</h1>
+  )
+}
+
+export default Success;
