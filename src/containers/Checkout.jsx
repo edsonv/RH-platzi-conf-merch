ac3d22e8f015a3c0e89a9e2133bf39f5c2083172
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-17 17:10:34 -0500

    ADD: maquetación y estilos de componente Checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import '../styles/components/Checkout.css';
 
 function Checkout() {
   return (
-    <h1>Checkout</h1>
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Lista de pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item name</h4>
+            <span>$18</span>
+          </div>
+          <button type='button'>Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type='button'>Continuar pedido</button>
+      </div>
+    </div>
   )
 }
 
