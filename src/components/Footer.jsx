917955c764bc578e978c2fbdd7ee255dc54593ed
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-17 16:21:48 -0500

    ADD:Layout y react-router-dom Se agrega componente Layout y se corrige manejo de rutas

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/components/Footer.css';
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">Platzi Conf Merch</p>
+      <p className="Footer-copy">Todos los izquierdos reservados</p>
+    </div>
+  )
+}
+
+export default Footer;
