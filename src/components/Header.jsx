759d8efdc0d9edcff9755be11c2bbe039015929a
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-18 00:51:36 -0500

    ADD: useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Header.css';
 
 function Header() {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
+
   return (
     <div className="Header">
       <h1 className="Header-title">
@@ -14,6 +18,7 @@ function Header() {
         <Link to="/checkout">
           <i className="fa-solid fa-basket-shopping" />
         </Link>
+        {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
       </div>
     </div>
   )
