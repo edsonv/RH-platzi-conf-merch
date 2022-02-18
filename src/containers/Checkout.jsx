fe7698a6d03da067c1dd5d71cd7507d8e440c15a
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-18 01:11:42 -0500

    ADD: useContext en Checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Checkout.css';
 
 function Checkout() {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+
+  const handleRemoveFromCart = product => () => {
+    removeFromCart(product);
+  }
+
+  const handleSumTotal = () => {
+    const reducer = (acc, cur) => acc + cur.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  }
+
   return (
     <div className="Checkout">
       <div className="Checkout-content">
-        <h3>Lista de pedidos:</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>Item name</h4>
-            <span>$18</span>
+        {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos</h3>}
+        {cart.map(item => (
+          <div className="Checkout-item">
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span>{item.price}</span>
+            </div>
+            <button type='button' onClick={handleRemoveFromCart(item)}>
+              <i className="fa-solid fa-trash-alt" />
+            </button>
           </div>
-          <button type='button'>
-            <i className="fa-solid fa-trash-alt" />
-          </button>
+        ))}
+      </div>
+      {cart.length > 0 && (
+        <div className="Checkout-sidebar">
+          <h3>{`Precio Total: $${handleSumTotal()}`}</h3>
+          <Link to="/checkout/information">
+            <button type='button'>Continuar pedido</button>
+          </Link>
         </div>
-      </div>
-      <div className="Checkout-sidebar">
-        <h3>Precio Total: $10</h3>
-        <Link to="/checkout/information">
-          <button type='button'>Continuar pedido</button>
-        </Link>
-      </div>
+      )}
     </div>
   )
 }
